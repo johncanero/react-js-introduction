6183ef6bdf5bff3027c2c6d1641b146d94e428a2
John Canero <johncanero.official@gmail.com>
2022-08-02 23:24:53 +0800

    Include Header.js from the Previous Project

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+
+    const name = "Jam";
+    const favoriteNumber = 23;
+  
+    
+  return (
+    <div>   
+            <header>
+                <div className="headerTitle" contentEditable='true' spellCheck='false'>
+                    <h1>Hello React World!</h1>
+                </div>
+            </header>
+          
+    
+            {/* <p>My favorite number is {favoriteNumber}.</p>  */}
+    </div>
+  )
+}
+
+export default Header
